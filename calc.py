b90a541394e6a357f2c60fe4f7183c7f12c662db
Fasil <fasil17062002@gmail.com>
2021-08-15 10:39:17 +0530

    frame added

--- calc.py
+++ calc.py
@@ -1,8 +1,18 @@
 import tkinter as tk
 
+GRAY = '#F0EAEA'
+
 window = tk.Tk()
 window.geometry('375x500')
-window.resizable(0, 0)
+window.resizable(0, 100)
 window.title('Calculator')
+
+display_frame = tk.Frame()
+display_frame = tk.Frame(window, bg=GRAY, height=100)
+display_frame.pack(expand=True, fill='both')
+
+button_frame = tk.Frame()
+button_frame = tk.Frame(window)
+button_frame.pack(expand=True, fill='both')
 
 window.mainloop()
