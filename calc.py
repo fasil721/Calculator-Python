a9efd973248177f77b4ca6f71ca1581f3db62965
Fasil <fasil17062002@gmail.com>
2021-08-15 15:21:41 +0530

    operator buttuns added

--- calc.py
+++ calc.py
@@ -1,11 +1,14 @@
 import tkinter as tk
 
+LABEL_COLOR = "#25265E"
+GRAY = '#F0EAEA'
+OFF_WHITE = "#F8FAFF"
+WHITE = "#FFFFFF"
+LIGHT_BLUE = "#CCEDFF"
 LARGE_FONT_STYLE = ("Arial", 40, "bold")
 SMALL_FONT_STYLE = ("Arial", 16)
 DIGITS_FONT_STYLE = ("Arial", 24, "bold")
-LABEL_COLOR = "#25265E"
-GRAY = '#F0EAEA'
-WHITE = "#FFFFFF"
+DEFAULT_FONT_STYLE = ("Arial", 20)
 
 window = tk.Tk()
 window.geometry('375x667')
@@ -34,9 +37,31 @@ digits = {
     1: (3, 1), 2: (3, 2), 3: (3, 3),
     0: (4, 2), '.': (4, 1)
 }
+operations = {"/": "\u00F7", "*": "\u00D7", "-": "-", "+": "+"}
 for digit, grid_value in digits.items():
     button = tk.Button(button_frame, text=str(digit), bg=WHITE, fg=LABEL_COLOR, font=DIGITS_FONT_STYLE,
                        borderwidth=0)
     button.grid(row=grid_value[0], column=grid_value[1], sticky=tk.NSEW)
+i = 0
+for operator, symbol in operations.items():
+    button = tk.Button(button_frame, text=symbol, bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE,
+                       borderwidth=0)
+    button.grid(row=i, column=4, sticky=tk.NSEW)
+    i += 1
+for x in range(1, 5):
+    button_frame.rowconfigure(x, weight=1)
+    button_frame.columnconfigure(x, weight=1)
+
+clear_button = tk.Button(button_frame, text="C", bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE,
+                         borderwidth=0)
+clear_button.grid(row=0, column=1, columnspan=2, sticky=tk.NSEW)
+
+delete_button = tk.Button(button_frame, text="\u232B", bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE,
+                          borderwidth=0)
+delete_button.grid(row=0, column=3, sticky=tk.NSEW)
+
+equal_button = tk.Button(button_frame, text='=', bg=LIGHT_BLUE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE,
+                         borderwidth=0)
+equal_button.grid(row=4, column=3, columnspan=2, sticky=tk.NSEW)
 
 window.mainloop()
