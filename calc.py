39dce67ad17c1fd7f3ee0ea51db0f3702b62618f
Fasil <fasil17062002@gmail.com>
2021-08-15 12:07:56 +0530

    label added

--- calc.py
+++ calc.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 
+LARGE_FONT_STYLE = ("Arial", 40, "bold")
+SMALL_FONT_STYLE = ("Arial", 16)
+LABEL_COLOR = "#25265E"
 GRAY = '#F0EAEA'
 
 window = tk.Tk()
@@ -12,5 +15,15 @@ display_frame.pack(expand=True, fill='both')
 
 button_frame = tk.Frame(window)
 button_frame.pack(expand=True, fill='both')
+
+total_expression = "0"
+current_expression = "0"
+display_total_label = tk.Label(display_frame, text=total_expression, anchor=tk.E, bg=GRAY,
+                               fg=LABEL_COLOR, padx=24, font=SMALL_FONT_STYLE)
+display_total_label.pack(expand=True, fill='both')
+
+display_label = tk.Label(display_frame, text=current_expression, anchor=tk.E, bg=GRAY,
+                         fg=LABEL_COLOR, padx=24, font=LARGE_FONT_STYLE)
+display_label.pack(expand=True, fill='both')
 
 window.mainloop()
