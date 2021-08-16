093f2f2d903cd02e321c9f3bf3a0d97e492a4d47
Fasil <fasil17062002@gmail.com>
2021-08-16 10:36:14 +0530

    Added buttons functionalities

--- calc.py
+++ calc.py
@@ -5,13 +5,13 @@ GRAY = '#F0EAEA'
 OFF_WHITE = "#F8FAFF"
 WHITE = "#FFFFFF"
 LIGHT_BLUE = "#CCEDFF"
-LARGE_FONT_STYLE = ("Arial", 40, "bold")
-SMALL_FONT_STYLE = ("Arial", 16)
-DIGITS_FONT_STYLE = ("Arial", 24, "bold")
+LARGE_FONT_STYLE = ("Arial", 35, "bold")
+SMALL_FONT_STYLE = ("Arial", 15)
+DIGITS_FONT_STYLE = ("Arial", 20, "bold")
 DEFAULT_FONT_STYLE = ("Arial", 20)
 
 window = tk.Tk()
-window.geometry('375x667')
+window.geometry('325x475')
 window.resizable(0, 0)
 window.title('Calculator')
 
@@ -21,8 +21,9 @@ display_frame.pack(expand=True, fill='both')
 button_frame = tk.Frame(window)
 button_frame.pack(expand=True, fill='both')
 
-total_expression = "0"
-current_expression = "0"
+total_expression = ''
+current_expression = ''
+
 display_total_label = tk.Label(display_frame, text=total_expression, anchor=tk.E, bg=GRAY,
                                fg=LABEL_COLOR, padx=24, font=SMALL_FONT_STYLE)
 display_total_label.pack(expand=True, fill='both')
@@ -38,14 +39,74 @@ digits = {
     0: (4, 2), '.': (4, 1)
 }
 operations = {"/": "\u00F7", "*": "\u00D7", "-": "-", "+": "+"}
+
+
+def update_label():
+    display_label.config(text=current_expression)
+
+
+def update_total_label():
+    display_total_label.config(text=total_expression)
+
+
+def add_to_expression(value):
+    global current_expression
+    current_expression += str(value)
+    update_label()
+
+
+def append_operator(operator):
+    global current_expression
+    global total_expression
+    current_expression += operator
+    total_expression += current_expression
+    current_expression = ''
+    update_total_label()
+    update_label()
+
+
+def evaluate():
+    global current_expression
+    global total_expression
+    total_expression += current_expression
+    update_total_label()
+
+    current_expression = str(eval(total_expression))
+    total_expression = ''
+    update_label()
+
+
+def clear():
+    global current_expression
+    global total_expression
+    current_expression = ''
+    total_expression = ''
+    update_total_label()
+    update_label()
+
+
+def delete():
+    global current_expression
+    global total_expression
+    if total_expression[:-1]:
+        total_expression = total_expression[:-1]
+        current_expression = total_expression
+        total_expression = ''
+        update_total_label()
+        update_label()
+    else:
+        current_expression = current_expression[:-1]
+        update_label()
+
+
 for digit, grid_value in digits.items():
-    button = tk.Button(button_frame, text=str(digit), bg=WHITE, fg=LABEL_COLOR, font=DIGITS_FONT_STYLE,
-                       borderwidth=0)
+    button = tk.Button(button_frame, text=digit, bg=WHITE, fg=LABEL_COLOR, font=DIGITS_FONT_STYLE,
+                       borderwidth=0, command=lambda n=digit: add_to_expression(n))
     button.grid(row=grid_value[0], column=grid_value[1], sticky=tk.NSEW)
 i = 0
 for operator, symbol in operations.items():
     button = tk.Button(button_frame, text=symbol, bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE,
-                       borderwidth=0)
+                       borderwidth=0, command=lambda o=operator: append_operator(o))
     button.grid(row=i, column=4, sticky=tk.NSEW)
     i += 1
 for x in range(1, 5):
@@ -53,15 +114,15 @@ for x in range(1, 5):
     button_frame.columnconfigure(x, weight=1)
 
 clear_button = tk.Button(button_frame, text="C", bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE,
-                         borderwidth=0)
+                         borderwidth=0, command=clear)
 clear_button.grid(row=0, column=1, columnspan=2, sticky=tk.NSEW)
 
 delete_button = tk.Button(button_frame, text="\u232B", bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE,
-                          borderwidth=0)
+                          borderwidth=0, command=delete)
 delete_button.grid(row=0, column=3, sticky=tk.NSEW)
 
 equal_button = tk.Button(button_frame, text='=', bg=LIGHT_BLUE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE,
-                         borderwidth=0)
+                         borderwidth=0, command=evaluate)
 equal_button.grid(row=4, column=3, columnspan=2, sticky=tk.NSEW)
 
 window.mainloop()
